afdd5e53f33f96a5c305fb3e4ed848c5f4d48ca7
smartzhao <smartzhao@tencent.com>
2022-04-28 13:13:13 +0800

    feat: add iris starter

---
+++ infra/base/iris-server.go
@@ -0,0 +1,60 @@
+package base
+
+import (
+	"resk/infra"
+	"time"
+
+	"github.com/kataras/iris/v12"
+	"github.com/kataras/iris/v12/middleware/logger"
+	irisRecover "github.com/kataras/iris/v12/middleware/recover"
+	"github.com/sirupsen/logrus"
+)
+
+var irisApplication *iris.Application
+
+type IrisServerStarter struct {
+	infra.BaseStarter
+}
+
+func Iris() *iris.Application {
+	return irisApplication
+}
+
+func (s *IrisServerStarter) Init(ctx infra.StarterContext) {
+	// 初始化 iris
+	irisApplication = initIris()
+	// 日志组件扩展
+	logger := irisApplication.Logger()
+	logger.Install(logrus.StandardLogger())
+}
+
+func (s *IrisServerStarter) Start(ctx infra.StarterContext) {
+	routes := Iris().GetRoutes()
+	for _, route := range routes {
+		logrus.Info(route.Method + route.Path)
+	}
+	port := ctx.Props().GetDefault("app.server.port", "18080")
+	Iris().Listen(":" + port)
+}
+
+func (s *IrisServerStarter) StartBlocking() bool {
+	return true
+}
+
+func initIris() *iris.Application {
+	app := iris.New()
+	app.UseRouter(irisRecover.New())
+	conf := logger.Config{
+		Status:     true,
+		IP:         true,
+		Method:     true,
+		Path:       true,
+		Query:      true,
+		TraceRoute: true,
+		LogFunc: func(endTime time.Time, latency time.Duration, status, ip, method, path string, message, headerMessage interface{}) {
+			app.Logger().Infof("| %s | %s | %s | %s | %s | %s | %s | %s |", endTime.Format("2006-01-02 15:04:05"), latency.String(), status, ip, method, path, message, headerMessage)
+		},
+	}
+	app.Use(logger.New(conf))
+	return app
+}
